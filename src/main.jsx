6b4a2246f6f14efa25226636c43e93e742b54768
ZaheerCodes <zaheermuhammad636@gmail.com>
2025-09-05 15:59:23 +0500

    Setting global css variables

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
+import './styles/global.css';
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
