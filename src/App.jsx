2c0165539981f7c6636c2538a39af57f3229d7b8
ZaheerCodes <zaheermuhammad636@gmail.com>
2025-09-05 21:56:21 +0500

    Renaming component folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import PostsList from './components/PostsList'
+import Button from './components/Button/Button'
+import Input from './components/Input/Input'
+import LoginForm from './features/LoginForm/LoginFrom';
+import SignupForm from './features/SignupForm/SignupForm'
+
 import './styles/App.css'
 
 function App() {
 
   return (
     <>
-      <PostsList />
+      <LoginForm />
+      {/* <SignupForm />       */}
+      {/* <PostsList /> */}
     </>
   )
 }
---
+++ src/features/SignupForm/SignupForm.jsx
@@ -0,0 +1,22 @@
+import styles from '../LoginForm/LoginFrom.module.css';
+import Input from '../../components/Input/Input'
+import Button from '../../components/Button/Button'
+
+const SignupForm = () => {
+    return (
+        <form className={styles.form}>
+            <h2 className={styles.heading}>Hello Again!</h2>
+            <p className={styles.subheading}>Welcome To SocialApp</p>
+            <Input type="text" placeholder="Email or username" required autoComplete="true" />
+            <Input type="password" placeholder="Password" required autoComplete="true" />
+            <Button id={styles.forgotLink} type="button" variant="hyperlink" label="Forgot Password?" size="sm" />
+            <Button type="submit" variant="primary" label="Login" size="md" />
+            <p id={styles.signupLink}>Don't have an account?
+                <Button type="button" variant="hyperlink" label="Sign up" size="sm" />
+            </p>
+        </form>
+    );
+
+}
+
+export default SignupForm;
